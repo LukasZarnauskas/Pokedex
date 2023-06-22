e0d66478f2d85c9f9c996bdf81c5f79a296f2c7b
GluteusMedius <zarnauskas97@gmail.com>
2023-06-22 10:36:11 +0300

    font changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import SearchPage from "./pages/SearchPage";
 import "./assets/App.css";
 function App() {
   return (
-    <div className="w-360 bg-primaryColor">
+    <div className="w-360 bg-primaryColor font-poppins">
       <SearchPage />
     </div>
   );
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -3,9 +3,13 @@ import React from "react";
 function SearchPage() {
   return (
     <div>
-      <h1 className="ml-4 pt-5 pb-3 flex text-2xl font-bold  text-white">
-        <img className="mr-4 " src="/src/image/Pokeball.png" alt="pokeball " />{" "}
-        Pokedex
+      <h1 className="ml-4 pt-5 pb-3 flex items-center text-2xl font-bold  text-white">
+        <img
+          className="mr-4 h-6 "
+          src="/src/image/Pokeball.png"
+          alt="pokeball "
+        />{" "}
+        Pokédex
       </h1>
     </div>
   );
