f3df152a656d813eb493ba8c51fb6fb49af9b929
GluteusMedius <zarnauskas97@gmail.com>
2023-06-27 01:03:51 +0300

    update

--- src/components/search/pokemonPage/Composition.jsx
+++ src/components/search/pokemonPage/Composition.jsx
@@ -5,14 +5,14 @@ function Composition({ info }) {
     <div className="flex justify-between items-center mx-auto mt-4 text-10  w-64">
       <div className=" text-center">
         <p className="flex text-grDark">
-          <img className="mr-1" src="/src/image/weight.png" alt="weight" />
+          <img className="mr-1" src="/image/weight.png" alt="weight" />
           {info.weight / 10} kg
         </p>
         <p className="text-grMedium text-8 mt-2.5">Weight</p>
       </div>
       <div className="border-x-2 px-4  text-center ">
         <p className="flex text-grDark">
-          <img className="mr-1" src="/src/image/straighten.png" alt="height" />
+          <img className="mr-1" src="/image/straighten.png" alt="height" />
           {info.height / 10} m
         </p>
         <p className="text-grMedium text-8 mt-2.5">Height</p>
